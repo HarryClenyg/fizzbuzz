47485652184b905b62efcda9845926f3e0564b03
Patryk Okpara <patrykokpara@hotmail.com>
2022-02-15 14:08:30 +0000

    6 tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,12 @@
 def fizzbuzz(number)
-  if number == 3
-    'fizz'
-  elsif number == 5
-    'buzz'
-  elsif number % 3 == 0 && number % 5 == 0
+  if number % 3 == 0 && number % 5 == 0
     'fizzbuzz'
-  end  
+  elsif  number % 3 == 0 
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
+  else
+    number
+  end
+   
 end
