ea53360038f83e39023ceec59f92c7f90ecc2e55
Harry <harrydawson90@hotmail.co.uk>
2022-02-15 13:51:10 +0000

    added fizzbuzz test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,9 @@
 def fizzbuzz(number)
   if number == 3
-    puts 'fizz'
+    'fizz'
   elsif number == 5
-    puts 'buzz'  
+    'buzz'
+  elsif number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
   end  
 end
